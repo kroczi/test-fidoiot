ce61079aae5a7b9bc2b14e27a4ba43dd299a90a3
Michał Krok <michal.krok@ibm.com>
2025-08-01 06:39:53 +0200

    logs

--- common/src/main/java/org/fidoalliance/fdo/test/common/TestProcess.java
+++ common/src/main/java/org/fidoalliance/fdo/test/common/TestProcess.java
@@ -118,6 +118,14 @@ public class TestProcess extends TestCase {
     try {
       Process process = builder.start();
 
+      try (BufferedReader reader = 
+          new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+          TestLogger.info("[docker] " + line);
+        }
+      }
+
       int exitCode = process.waitFor();
       TestLogger.info("Process exited with code: " + exitCode);
     } catch (Exception e) {
