fd088b614374c53ef1aa10a189b0e3e46b303794
Michał Krok <michal.krok@ibm.com>
2025-08-01 06:55:00 +0200

    wip

--- common/src/main/java/org/fidoalliance/fdo/test/common/TestProcess.java
+++ common/src/main/java/org/fidoalliance/fdo/test/common/TestProcess.java
@@ -103,7 +103,7 @@ public class TestProcess extends TestCase {
     List<String> commands = new ArrayList<>();
     commands.add("bash");
     commands.add("-c");
-    commands.add(command);
+    commands.add("docker compose up -d --build ; docker compose logs --no-color");
 
     ProcessBuilder builder = new ProcessBuilder();
     builder.command(commands);
